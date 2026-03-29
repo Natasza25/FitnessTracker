177ae360ebad060216ceb0675961dbbc816c922a
Shn3r <sb.sarysz@gmail.com>
2026-03-29 15:47:11 +0200

    synchro

--- src/main/java/pl/wsb/fitnesstracker/user/api/HealthMetrics.java
+++ src/main/java/pl/wsb/fitnesstracker/user/api/HealthMetrics.java
@@ -1,34 +1,34 @@
 package pl.wsb.fitnesstracker.user.api;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
+import lombok.*;
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "health_metrics")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class HealthMetrics {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "user_id", nullable = false)
-    private Long userId;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     @Column(nullable = false)
     private LocalDate date;
 
+    @Column(nullable = false)
     private Double weight;
 
     private Double height;
 
     @Column(name = "heart_rate")
     private Integer heartRate;
-
 }
